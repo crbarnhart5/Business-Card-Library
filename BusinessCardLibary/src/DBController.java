4790c3d73416ad4a21c66122670b0b7ef71cb087
Connor Barnhart <crbarnhart5@gmail.com>
2018-08-07 22:04:00 -0400

    Classes and method structure implemented

---
+++ BusinessCardLibary/src/DBController.java
@@ -0,0 +1,28 @@
+
+public class DBController {
+	public DBController(){
+		
+	}
+	
+	public static String View(String info){
+		
+		return "";
+	}
+	
+	public static void Add(String info){
+		
+	}
+	
+	public static String Update(String info){
+		
+		return "";
+	}
+	
+	public static void Delete(String info){
+		
+	}
+	
+	public static void Exit(){
+		
+	}
+}
---
+++ BusinessCardLibary/src/ProgramController.java
@@ -0,0 +1,28 @@
+
+public class ProgramController {
+	public static void Main(String[] args){
+		System.out.println("Hello, please select your choice");
+		InteractWithUser();
+		System.out.println("Goodbye");
+	}
+	
+	private static void InteractWithUser(){
+		
+	}
+	
+	private static void View(){
+		
+	}
+	
+	private static void Add(){
+		
+	}
+	
+	private static void Update(){
+		
+	}
+	
+	private static void Delete(){
+		
+	}
+}
