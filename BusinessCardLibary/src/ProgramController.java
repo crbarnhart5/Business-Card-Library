dd4bcddc64d1a8b6f08fc43875f5d5042ba71525
Connor Barnhart <crbarnhart5@gmail.com>
2018-08-07 22:24:52 -0400

    Implemented class InteractWithUser functionality

--- BusinessCardLibary/src/ProgramController.java
+++ BusinessCardLibary/src/ProgramController.java
@@ -1,3 +1,5 @@
+import java.awt.image.BufferedImage;
+import java.util.Scanner;
 
 public class ProgramController {
 	public static void Main(String[] args){
@@ -7,7 +9,32 @@ public class ProgramController {
 	}
 	
 	private static void InteractWithUser(){
-		
+		boolean loop = true;
+		String initialMessage = "Please enter number of option"
+								+ "1: View contact info"
+								+ "2: Add contact info"
+								+ "3: Update contact info"
+								+ "4: Delete contact info"
+								+ "5: Exit";
+		Scanner sc = new Scanner(System.in);
+		do{
+			System.out.println(initialMessage);
+			String choice = sc.nextLine();
+			switch(choice){
+				case "1": View();
+					break;
+				case "2": Add();
+					break;
+				case "3": Update();
+					break;
+				case "4": Delete();
+					break;
+				case "5": return;
+				default: System.out.println("Invalid choice entered");
+					break;
+			}
+			
+		} while(loop);
 	}
 	
 	private static void View(){
@@ -24,5 +51,10 @@ public class ProgramController {
 	
 	private static void Delete(){
 		
+	}
+	
+	private static BufferedImage TakePhoto(){
+		
+		return null;
 	}
 }
